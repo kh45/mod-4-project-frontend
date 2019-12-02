11cecb540e3d632c53733448a56b20021dd8ab51
kh45 <khaledhassan45@gmail.com>
2019-12-02 17:23:39 -0500

    switch between logged in and out

--- nightlife-client/src/App.js
+++ nightlife-client/src/App.js
@@ -3,14 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/login'
 import Navbar from './components/navbar'
+import Search from './components/search'
 
-function App() {
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      logged_in: false
+    }
+  }
+
+  logIn = () => {
+    this.setState({
+      logged_in: true
+    })
+  }
+  
+  render() {
+
+  
   return (
     <div className="App">
       {/* <Navbar /> */}
-      <Login />
+      {this.state.logged_in ? <Search /> : <Login logIn={this.logIn} />}
     </div>
   );
+  }
 }
-
-export default App;
