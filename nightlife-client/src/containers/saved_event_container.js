44adc0f5896577d1db2276be8c04ff6c96e8c680
David Fellows <dfellows12@gmail.com>
2019-12-04 12:27:52 -0500

    Styling, savedeventcontainer

--- nightlife-client/src/containers/saved_event_container.js
+++ nightlife-client/src/containers/saved_event_container.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import SavedEventCard from '../components/savedeventcard'
 
 class SavedEventContainer extends Component {
-
+  generateCards = (array) => array.map(event => <SavedEventCard event={event}/>)
   render() {
     return (
         <div>
-            
+          <div className="cardContainer">
+            {this.generateCards(this.props.cards)}
+          </div>
         </div>
     )
   }
